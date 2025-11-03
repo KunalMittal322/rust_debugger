e528abb9e45045137dbb434ea40543f8e6512743
KunalMittal322 <mittal.kunal01@gmail.com>
2025-11-02 20:43:29 -0800

    Idk wat I am doing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,97 @@
+use windows_sys::{
+    Win32::Foundation::*,
+    Win32::System::Environment::*,
+    Win32::System::{Diagnostics::Debug::*, Threading::*, WindowsProgramming::INFINITE},
+};
+
+use std::ptr::null;
+
+fn wcslen(ptr: *const u16) -> usize {
+    let mut len = 0;
+    unsafe {
+        while *ptr.add(len) != 0{
+            len += 1;
+        }
+    }
+    len
+}
+
+fn show_usage(error_message: &str) {
+    println!("Error: {msg}", msg=error_message);
+    println!("Usage: DbgRs<Command Line>");
+}
+
+fn parse_command_line() -> Result<Vec<u16>, &'static str> {
+    let cmd_line = unsafe {
+        let p = GetCommandLineW();
+        let len = wcslen(p);
+        std::slice::from_raw_parts(p, len + 1)
+    };
+    let mut cmd_line_iter = cmd_line.iter().copied();
+
+    let first = cmd_line_iter.next().ok_or("Command line was empty")?;
+
+    let end_char = (if first == '"' as u16 {'"'} else {' '}) as u16;
+
+    loop{
+        let next = cmd_line_iter.next().ok_or("No arguments found");
+        if next == end_char {
+            break;
+        }
+    }
+    let cmd_line_iter = cmd_line_iter.skip_while(|x| x == &(' ' as u16));
+    Ok(cmd_line_iter.collect())
+}
+
+fn main_debugger_loop() {
+    loop{
+        let mut debug_event: DEBUG_EVENT = unsafe {std::mem:zeroed()};
+        unsafe {
+            WaitForDebugEvent(&mut debug_event, INFINITE);
+        }
+        match debug_event.dwDebugEventCode {
+            EXCEPTION_DEBUG_EVENT => println!("Exception"),
+            CREATE_THREAD_DEBUG_EVENT => println!("CreateThread"),
+            CREATE_PROCESS_DEBUG_EVENT => println!("CreateProcess"),
+            EXIT_THREAD_DEBUG_EVENT => println!("ExitThread"),
+            EXIT_PROCESS_DEBUG_EVENT => println!("ExitProcess"),
+            LOAD_DLL_DEBUG_EVENT => println!("LoadDll"),
+            UNLOAD_DLL_DEBUG_EVENT => println!("UnloadDll"),
+            OUTPUT_DEBUG_STRING_EVENT => println!("OutputDebugString"),
+            RIP_EVENT => println!("RipEvent"),
+            _ => panic!("Unexpected debug event"),
+        }
+
+        if debug_event.dwDebugEventCode == EXIT_PROCESS_DEBUG_EVENT {
+            break;
+        }
+
+        unsafe {
+            ContinueDebugEvent(
+                debug_event.dwProcessId,
+                debug_event.dwThreadId,
+                DBG_EXCEPTION_NOT_HANDLED,
+            );
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let target_command_line_result = parse_command_line();
+
+    let mut process_id: PROCESS_INFORMATION = unsafe { std::mem::zeroed() };
+    unsafe {
+        CreateProcessW(
+            null(),
+            null(),
+            null(),
+            null(),
+            null(),
+            null(),
+            null(),
+            null(),
+            null(),
+            null(),
+        );
+    }
 }
